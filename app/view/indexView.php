0bed788bc1807057177532038eb1b2d1d5598063
farewell1994 <andresk1994@ukr.net>
2017-07-15 11:17:19 +0300

    add Twig

--- app/view/indexView.php
+++ app/view/indexView.php
@@ -1,1 +1,8 @@
 <p>ok</p>
+<?php
+require_once './vendor/autoload.php';
+
+$loader = new Twig_Loader_String();
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien'));
