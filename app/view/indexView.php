fefab251b0e6860a6bd768fed59c7d56710e8222
farewell1994 <andresk1994@ukr.net>
2017-07-16 15:59:34 +0300

    data output

--- app/view/indexView.php
+++ app/view/indexView.php
@@ -5,4 +5,12 @@ require_once './vendor/autoload.php';
 $loader = new Twig_Loader_String();
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien'));
+echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien'))."<br>";
+echo "<ol>";
+foreach($data as $students){
+	echo "<li>";
+	echo "Name = ".$students[1]."<br>";
+	echo "Age = ".$students[2]."<br>";
+	echo "</li>";
+}
+echo "</ol>";
