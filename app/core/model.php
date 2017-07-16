3b03aef41bafc72e3aacd1526634573fa84d11d0
farewell1994 <andresk1994@ukr.net>
2017-07-16 13:43:42 +0300

    connect db

--- app/core/model.php
+++ app/core/model.php
@@ -1,6 +1,9 @@
 <?php
 class Model
 {
+	function __construct(){
+		$connect = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+	}
 	public function getTable()
 	{
 	}
--- index.php
+++ index.php
@@ -2,4 +2,3 @@
 ini_set('display_errors', 1);
 require_once 'app/bootstrap.php';
 require_once 'app/config.php';
-$connect = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
