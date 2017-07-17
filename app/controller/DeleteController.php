0288454ee993a930a5a99f3d48b54943c660d7ec
farewell1994 <andresk1994@ukr.net>
2017-07-18 00:04:05 +0300

    add deletion of fields

--- app/controller/DeleteController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class DeleteController extends Controller
-{
-	function deleteRow($id){
-		$this->model = new MainModel();
-		$data = $this->model->deleteInfo();
-		header('Location: test');
-	}
-}
