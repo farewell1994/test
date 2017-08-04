a2abeb54f9c2991ff8dd4186b43e723c0692e60f
farewell1994 <andresk1994@ukr.net>
2017-08-04 20:30:21 +0300

    improved indexView

--- src/View/indexView.php
+++ src/View/indexView.php
@@ -1,9 +1,10 @@
 {% include 'head.html' %}
     <body>
+        <a class="add" href="/test/main/add">Add Student</a>
         <ol>
             {% for info in data %}
-
-            <li>
+            <hr>
+            <li class="list">
                 <p>Name = {{ info.name }}</p>
                 <p>Age = {{ info.age }}</p>
                 <a href="main/edit/{{ info.id }}-{{ info.name }}-{{ info.age }}">edit</a> | <a href="main/delete/{{ info.id }}">delete</a>
@@ -11,6 +12,5 @@
             {% endfor %}
 
         </ol>
-        <a href="/test/main/add">Add Student</a>
     </body>
 </html>
